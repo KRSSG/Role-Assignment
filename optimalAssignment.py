0b59303457b311ce3d22ea6114b6516bd431b7bc
VISHAL-RAJ-RAJ <35928662+VISHAL-RAJ-RAJ@users.noreply.github.com>
2018-05-24 15:34:13 +0530

    optimal_assignment.py
    made some minor changes..

--- optimalAssignment.py
+++ optimalAssignment.py
@@ -25,16 +25,16 @@ class optimalAssignment(object):
 		# thus exhaustion is optimal as no_of_bot <= 6
 		bot_Comb = list(combinations(botL, no_of_task))
 		task_Comb = list(permutations(taskD.keys(), no_of_task))
-		max_val = 99999
-		for x in bot_comb:
-			for y in task_comb:
+		min_val = 99999
+		for x in bot_Comb:
+			for y in task_Comb:
 				comb = zip(x, y)
 				comb.sort( key = lambda a : self.Mat[a[0]][a[1]] )
-				if min_val > Mat[comb[-1][0]][comb[-1][1]]:
-					min_val = Mat[comb[-1][0]][comb[-1][1]]
-					bot_comb_index = bot_comb.index(x)
-					task_comb_index = task_comb.index(y)
-		return zip( bot_comb[bot_comb_index], task_comb[task_comb_index] )
+				if min_val > self.Mat[comb[-1][0]][comb[-1][1]]:
+					min_val = self.Mat[comb[-1][0]][comb[-1][1]]
+					bot_comb_index = bot_Comb.index(x)
+					task_comb_index = task_Comb.index(y)
+		return zip( bot_Comb[bot_comb_index], task_Comb[task_comb_index] )
 					
 				
 		
